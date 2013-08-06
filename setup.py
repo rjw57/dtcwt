e7edda5437f4259e63a2fbb8fa0439aeb16bdaf7
Rich Wareham <rjw57@cam.ac.uk>
2013-08-06 12:11:39 +0100

    initial version

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "dtcwt",
+    version = "0.0.1",
+    author = "Rich Wareham",
+    author_email = "rich.dtcwt@richwareham.com",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)

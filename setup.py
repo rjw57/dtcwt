d71767fc17597f927b53a051c2ee78fcac1ebe56
Rich Wareham <rjw57@cam.ac.uk>
2013-08-08 00:37:55 +0100

    start development for 0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = 'dtcwt',
-    version = '0.3',
+    version = '0.4dev1',
     author = "Rich Wareham",
     author_email = "rich.dtcwt@richwareham.com",
     description = ("A port of the Dual-Tree Complex Wavelet Transform MATLAB toolbox."),

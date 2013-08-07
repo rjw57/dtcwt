01ecc41d58f1fab12341ee5c577d616bdbf003dc
Rich Wareham <rjw57@cam.ac.uk>
2013-08-07 23:54:37 +0100

    fix relative imports for Python3

--- dtcwt/__init__.py
+++ dtcwt/__init__.py
@@ -1,6 +1,6 @@
-from coeffs import biort, qshift
-from transform1d import dtwavexfm, dtwaveifm
-from transform2d import dtwavexfm2, dtwaveifm2
+from .coeffs import biort, qshift
+from .transform1d import dtwavexfm, dtwaveifm
+from .transform2d import dtwavexfm2, dtwaveifm2
 
 __all__ = [
     'dtwavexfm',

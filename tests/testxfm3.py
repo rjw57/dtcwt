3e843fb547bafc4c8fa00c10a4968d687ad45e44
Rich Wareham <rjw57@cam.ac.uk>
2013-08-08 10:56:30 +0100

    start test suite for 3d transform

---
+++ tests/testxfm3.py
@@ -0,0 +1,24 @@
+import os
+from nose.tools import raises
+from nose.plugins.attrib import attr
+
+import numpy as np
+
+def setup():
+    global sphere
+    X, Y, Z = np.meshgrid(np.arange(-63,64), np.arange(-63,64), np.arange(-63,64))
+
+    r = np.sqrt(X*X + Y*Y + Z*Z)
+    sphere = np.where(r <= 55, 1.0, 0.0)
+
+def test_sphere():
+    # Check general aspects of sphere are OK
+    assert sphere.shape == (127,127,127)
+    assert sphere.min() >= 0
+    assert sphere.max() <= 1
+
+    # Check volume of sphere is ok to within 5%
+    sphere_vol = (4.0/3.0) * np.pi * 55*55*55
+    assert np.abs(np.sum(sphere.flatten()) - sphere_vol) < 5e-2*sphere_vol
+
+# vim:sw=4:sts=4:et

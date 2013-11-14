b86ac339f5690f091b3231105bb2a0d73db7e577
scf32 <scf32@cam.ac.uk>
2013-11-14 12:49:53 +0000

    removed 2b functions from init as they are deprecated

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .coeffs import biort, qshift
 from .transform1d import dtwavexfm, dtwaveifm
-from .transform2d import dtwavexfm2, dtwaveifm2, dtwavexfm2b, dtwaveifm2b
+from .transform2d import dtwavexfm2, dtwaveifm2
 from .transform3d import dtwavexfm3, dtwaveifm3
 
 __all__ = [

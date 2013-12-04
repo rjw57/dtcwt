a75486de36a33d2a29c9b9e178a71b62d4f00294
Rich Wareham <rjw57@cam.ac.uk>
2013-12-04 15:28:11 +0000

    add output from cpxinterp2b to 'known good' output

--- matlab/gen_verif.m
+++ matlab/gen_verif.m
@@ -30,4 +30,20 @@ qshift_b_bp = load('qshift_b_bp');
 
 [lena_Ylb, lena_Yhb, lena_Yscaleb] = dtwavexfm2b(lena, 4, 'near_sym_b_bp', 'qshift_b_bp');
 
-save('verification.mat', 'lena_coldfilt', 'lena_colifilt', 'lena_Yl', 'lena_Yh', 'lena_Yscale', 'lena_Ylb', 'lena_Yhb', 'lena_Yscaleb');
+%% 3x interpolation of subbands
+x = lena_Yh{3};
+sx = size(x);
+nx = sx(2)*3; ny = sx(1)*3;
+scx = sx(2) / nx;
+scy = sx(1) / ny;
+[X, Y] = meshgrid(0.5 + ((1:nx)-0.5)*scx, 0.5 + ((1:ny)-0.5)*scy);
+locs = [Y(:), X(:)];
+w = [-3 -1; -3 -3; -1 -3; 1 -3; 3 -3; 3 -1]*pi/2.15;
+lena_upsample = zeros(ny, nx, 6);
+for sb=1:6
+    tmp = zeros(ny, nx);
+    tmp(:) = cpxinterp2b(x(:,:,sb),locs,w(sb,:), 'linear');
+    lena_upsample(:,:,sb) = tmp;
+end
+
+save('verification.mat', 'lena_coldfilt', 'lena_colifilt', 'lena_Yl', 'lena_Yh', 'lena_Yscale', 'lena_Ylb', 'lena_Yhb', 'lena_Yscaleb', 'lena_upsample');

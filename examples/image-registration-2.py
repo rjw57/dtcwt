86c2e2d1811af33fc38ce3ab03730f218b8be650
Rich Wareham <rjw57@cam.ac.uk>
2014-01-27 15:50:56 +0000

    add tennis imagery to registration example

--- examples/image-registration-2.py
+++ examples/image-registration-2.py
@@ -21,49 +21,56 @@ from dtcwt.registration import *
 
 logging.basicConfig(level=logging.INFO)
 
-# Load test images
-logging.info('Loading frames')
-test_frames = np.load(os.path.join(os.path.dirname(__file__), '..', 'tests', 'traffic.npz'))
-f1 = test_frames['f1']
-f2 = test_frames['f2']
+def register_frames(filename):
+    # Load test images
+    logging.info('Loading frames from "{0}"'.format(filename))
+    test_frames = np.load(filename)
+    f1 = test_frames['f1']
+    f2 = test_frames['f2']
 
-# Take the DTCWT of both frames.
-logging.info('Taking DTCWT')
-nlevels = 6
-Yl1, Yh1 = dtcwt.dtwavexfm2(f1, nlevels=nlevels)
-Yl2, Yh2 = dtcwt.dtwavexfm2(f2, nlevels=nlevels)
+    # Take the DTCWT of both frames.
+    logging.info('Taking DTCWT')
+    nlevels = 6
+    Yl1, Yh1 = dtcwt.dtwavexfm2(f1, nlevels=nlevels)
+    Yl2, Yh2 = dtcwt.dtwavexfm2(f2, nlevels=nlevels)
 
-# Solve for transform
-logging.info('Finding flow')
-avecs = estimateflow(Yh1, Yh2)
+    # Solve for transform
+    logging.info('Finding flow')
+    avecs = estimateflow(Yh1, Yh2)
 
-logging.info('Computing warped image')
-warped_f1 = warp(f1, avecs, method='bilinear')
+    logging.info('Computing warped image')
+    warped_f1 = warp(f1, avecs, method='bilinear')
 
-logging.info('Computing velocity field')
-step = 8
-X, Y = np.meshgrid(np.arange(f1.shape[1]), np.arange(f1.shape[0]))
-vxs, vys = velocityfield(avecs, f1.shape, method='bilinear')
+    logging.info('Computing velocity field')
+    step = 8
+    X, Y = np.meshgrid(np.arange(f1.shape[1]), np.arange(f1.shape[0]))
+    vxs, vys = velocityfield(avecs, f1.shape, method='bilinear')
 
-figure()
-subplot(221)
-imshow(np.dstack((f1, f2, np.zeros_like(f1))))
-title('Overlaid frames')
+    vxs -= np.median(vxs.flat)
+    vys -= np.median(vys.flat)
 
-subplot(222)
-imshow(np.dstack((warped_f1, f2, np.zeros_like(f2))))
-title('Frame 1 warped to Frame 2 (image domain)')
+    figure()
+    subplot(221)
+    imshow(np.dstack((f1, f2, np.zeros_like(f1))))
+    title('Overlaid frames')
 
-subplot(223)
-imshow(np.dstack((f1, f2, np.zeros_like(f2))))
-quiver(X[::step,::step], Y[::step,::step],
-        -vxs[::step,::step]*f1.shape[1], -vys[::step,::step]*f1.shape[0],
-        color='b', angles='xy', scale_units='xy', scale=1)
-title('Computed velocity field')
+    subplot(222)
+    imshow(np.dstack((warped_f1, f2, np.zeros_like(f2))))
+    title('Frame 1 warped to Frame 2 (image domain)')
 
-subplot(224)
-imshow(np.sqrt(vxs*vxs + vys*vys), interpolation='none', cmap=cm.hot)
-colorbar()
-title('Magnitude of computed velocity')
+    subplot(223)
+    imshow(np.dstack((f1, f2, np.zeros_like(f2))))
+    quiver(X[::step,::step], Y[::step,::step],
+            -vxs[::step,::step]*f1.shape[1], -vys[::step,::step]*f1.shape[0],
+            color='b', angles='xy', scale_units='xy', scale=1)
+    title('Computed velocity field (median subtracted)')
+
+    subplot(224)
+    imshow(np.sqrt(vxs*vxs + vys*vys), interpolation='none', cmap=cm.hot)
+    colorbar()
+    title('Magnitude of computed velocity (median subtracted)')
+
+register_frames(os.path.join(os.path.dirname(__file__), '..', 'tests', 'traffic.npz'))
+register_frames(os.path.join(os.path.dirname(__file__), '..', 'tests', 'tennis.npz'))
 
 show()

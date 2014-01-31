e02ab9883fed0e587282da0cc660b12a15e972a3
Rich Wareham <rjw57@cam.ac.uk>
2014-01-31 14:29:00 +0000

    tests: add lena loading function

--- tests/datasets.py
+++ tests/datasets.py
@@ -5,3 +5,7 @@ def regframes(name):
     """Load the *name* registration dataset and return source and reference frame."""
     frames = np.load(os.path.join(os.path.dirname(__file__), name + '.npz'))
     return frames['f1'], frames['f2']
+
+def lena():
+    """Return Lena in all her glory."""
+    return np.load(os.path.join(os.path.dirname(__file__), 'lena.npz'))['lena']

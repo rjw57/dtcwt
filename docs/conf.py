7352545fc89b2edc794006de9fc3fbe6a66e8a2a
Rich Wareham <rjw57@cam.ac.uk>
2014-01-31 13:31:55 +0000

    docs: add ipython directive extension
    Add an extension providing direct execution of Python code samples along
    with the ability to embed plots.

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,14 @@ sys.path.insert(0, os.path.abspath(os.path.join('..', 'tests')))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.mathjax']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'matplotlib.sphinxext.ipython_directive',
+    'IPython.sphinxext.ipython_console_highlighting',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

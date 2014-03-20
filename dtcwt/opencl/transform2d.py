5e4b1d948ca824871a21e47394c167bcae57b1dd
Rich Wareham <rjw57@cam.ac.uk>
2014-03-20 15:48:15 +0000

    opencl: add queue explicitly to q2c call

--- dtcwt/opencl/transform2d.py
+++ dtcwt/opencl/transform2d.py
@@ -217,6 +217,7 @@ class Transform2d(Transform2dNumPy):
                 axis_convolve(Hi,h0o,axis=1,queue=queue),
                 axis_convolve(Lo,h1o,axis=1,queue=queue),
                 diag,
+                queue=queue
             )
 
             if include_scale:
@@ -253,6 +254,7 @@ class Transform2d(Transform2dNumPy):
                 axis_convolve_dfilter(Hi,h0b,axis=1,queue=queue),
                 axis_convolve_dfilter(Lo,h1b,axis=1,queue=queue),
                 diag,
+                queue=queue
             )
 
             if include_scale:

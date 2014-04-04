2fe5c6705c299f8d1263d1795c7b3fd38676d9d3
Rich Wareham <rjw57@cam.ac.uk>
2014-04-04 13:56:58 +0100

    add test to cross-check OpenCL sampling with NumPy

--- tests/testopenclsampling.py
+++ tests/testopenclsampling.py
@@ -1,6 +1,7 @@
 import dtcwt
 import dtcwt.opencl.sampling
-from dtcwt.sampling import rescale
+import dtcwt.numpy.sampling as npsample
+from dtcwt.sampling import rescale, sample
 
 from .util import skip_if_no_cl
 
@@ -20,6 +21,20 @@ def test_correct_backend():
     assert dtcwt._sampling is dtcwt.opencl.sampling
 
 @skip_if_no_cl
+def test_rescale_pixel_centre():
+    # Create random 100x120 image
+    X = np.random.rand(100,120)
+
+    # Re size up
+    Xrs = rescale(X, (200, 240), 'nearest')
+    assert Xrs.shape == (200, 240)
+
+    # Output should be 4x4 blocks identical to original
+    for dx, dy in ((0,0), (0,1), (1,0), (1,1)):
+        Y = Xrs[dx::2,dy::2]
+        assert np.all(np.abs(X-Y) < 1e-8)
+
+@skip_if_no_cl
 def test_rescale_nearest():
     # Create random 100x120 image
     X = np.random.rand(100,120)
@@ -36,15 +51,22 @@ def test_rescale_nearest():
     assert np.all(np.abs(X-Xrecon) < 1e-8)
 
 @skip_if_no_cl
-def test_rescale_pixel_centre():
+def test_nearest_against_numpy():
     # Create random 100x120 image
     X = np.random.rand(100,120)
 
-    # Re size up
-    Xrs = rescale(X, (200, 240), 'nearest')
-    assert Xrs.shape == (200, 240)
+    # Generate random sampling co-ords
+    sample_shape = (320, 240)
+    ys = (4 * np.random.rand(*sample_shape) - 2) * X.shape[0]
+    xs = (4 * np.random.rand(*sample_shape) - 2) * X.shape[1]
 
-    # Output should be 4x4 blocks identical to original
-    for dx, dy in ((0,0), (0,1), (1,0), (1,1)):
-        Y = Xrs[dx::2,dy::2]
-        assert np.all(np.abs(X-Y) < 1e-8)
+    # Sample
+    Xsampled = sample(X, xs, ys, 'nearest')
+    assert Xsampled.shape == sample_shape
+
+    # Sample via numpy
+    Xsampled_np = npsample.sample_nearest(X, xs, ys)
+    assert Xsampled_np.shape == Xsampled.shape
+
+    # Sampling should agree to within tolerance
+    assert np.all(np.abs(Xsampled-Xsampled_np) < 1e-6)

3988afe5351f933360987a1b17d33a218a7fe994
Rich Wareham <rjw57@cam.ac.uk>
2014-04-04 17:38:34 +0100

    opencl: add further tests against numpy

--- tests/testopenclsampling.py
+++ tests/testopenclsampling.py
@@ -23,7 +23,7 @@ def test_correct_backend():
 @skip_if_no_cl
 def test_rescale_pixel_centre():
     # Create random 100x120 image
-    X = np.random.rand(100,120)
+    X = np.random.rand(100,120).astype(np.float32)
 
     # Re size up
     Xrs = rescale(X, (200, 240), 'nearest')
@@ -37,7 +37,7 @@ def test_rescale_pixel_centre():
 @skip_if_no_cl
 def test_rescale_nearest():
     # Create random 100x120 image
-    X = np.random.rand(100,120)
+    X = np.random.rand(100,120).astype(np.float32)
 
     # Re size up
     Xrs = rescale(X, (200, 240), 'nearest')
@@ -51,9 +51,9 @@ def test_rescale_nearest():
     assert np.all(np.abs(X-Xrecon) < 1e-8)
 
 @skip_if_no_cl
-def test_nearest_against_numpy():
+def test_nearest_against_numpy_2d():
     # Create random 100x120 image
-    X = np.random.rand(100,120)
+    X = np.random.rand(100,120).astype(np.float32)
 
     # Generate random sampling co-ords
     sample_shape = (320, 240)
@@ -63,6 +63,27 @@ def test_nearest_against_numpy():
     # Sample
     Xsampled = sample(X, xs, ys, 'nearest')
     assert Xsampled.shape == sample_shape
+
+    # Sample via numpy
+    Xsampled_np = npsample.sample_nearest(X, xs, ys)
+    assert Xsampled_np.shape == Xsampled.shape
+
+    # Sampling should agree to within tolerance
+    assert np.all(np.abs(Xsampled-Xsampled_np) < 1e-6)
+
+@skip_if_no_cl
+def test_nearest_against_numpy_3d():
+    # Create random 100x120x4 image
+    X = np.random.rand(100,120,4)
+
+    # Generate random sampling co-ords
+    sample_shape = (320, 240)
+    ys = (4 * np.random.rand(*sample_shape) - 2) * X.shape[0]
+    xs = (4 * np.random.rand(*sample_shape) - 2) * X.shape[1]
+
+    # Sample
+    Xsampled = sample(X, xs, ys, 'nearest')
+    assert Xsampled.shape == sample_shape + X.shape[2:]
 
     # Sample via numpy
     Xsampled_np = npsample.sample_nearest(X, xs, ys)

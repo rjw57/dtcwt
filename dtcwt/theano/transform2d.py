626d432466083e40c9f5be78d5c1c3092577d470
Rich Wareham <rjw57@cam.ac.uk>
2014-07-01 12:30:34 +0100

    correct runtime errors in stub theano backend
    Mostly these are silly copy-paste errors from the OpenCL backend.

--- dtcwt/theano/transform2d.py
+++ dtcwt/theano/transform2d.py
@@ -1,3 +1,12 @@
 import theano.tensor as T
 
+from dtcwt.defaults import DEFAULT_BIORT, DEFAULT_QSHIFT
 from dtcwt.numpy import Transform2d, Pyramid
+
+def dtwavexfm2(X, nlevels=3, biort=DEFAULT_BIORT, qshift=DEFAULT_QSHIFT, include_scale=False):
+    t = Transform2d(biort=biort, qshift=qshift)
+    r = t.forward(X, nlevels=nlevels, include_scale=include_scale)
+    if include_scale:
+        return r.lowpass, r.highpasses, r.scales
+    else:
+        return r.lowpass, r.highpasses

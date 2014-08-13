61002d7238a11eae8e916bd191f7538e08072276
Rich Wareham <rjw57@cam.ac.uk>
2014-08-13 15:32:50 +0100

    add traffic image to datasets

--- tests/datasets.py
+++ tests/datasets.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import PIL.Image as Image
 
 def regframes(name):
     """Load the *name* registration dataset and return source and reference frame."""
@@ -9,3 +10,8 @@ def regframes(name):
 def lena():
     """Return Lena in all her glory."""
     return np.load(os.path.join(os.path.dirname(__file__), 'lena.npz'))['lena']
+
+def traffic_hd():
+    """Return a 1080p image of some traffic."""
+    return np.asarray(Image.open(os.path.join(os.path.dirname(__file__), 'traffic_hd.jpg')))
+

907fdfcef3dd10c7960c675a11b3872dd2e016f6
Rich Wareham <rjw57@cam.ac.uk>
2014-08-14 12:20:10 +0100

    allow loading of traffic image in RGB or grayscale

--- tests/datasets.py
+++ tests/datasets.py
@@ -12,6 +12,12 @@ def lena():
     return np.load(os.path.join(os.path.dirname(__file__), 'lena.npz'))['lena']
 
 def traffic_hd():
-    """Return a 1080p image of some traffic."""
-    return np.asarray(Image.open(os.path.join(os.path.dirname(__file__), 'traffic_hd.jpg')))
+    """Return a 1080p image of some traffic as a floating point greyscale image."""
+    return np.asarray(Image.open(
+        os.path.join(os.path.dirname(__file__), 'traffic_hd.jpg')).convert('L')) / 255.0
+
+def traffic_hd_rgb():
+    """Return a 1080p image of some traffic as a floating point RGB image."""
+    return np.asarray(Image.open(
+        os.path.join(os.path.dirname(__file__), 'traffic_hd.jpg'))) / 255.0
 

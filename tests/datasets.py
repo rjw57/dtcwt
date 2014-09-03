c5c5047ecad5a33a1a9b880c3333077a5232373b
Rich Wareham <rjw57@cam.ac.uk>
2014-09-03 11:47:56 +0100

    add traffic image dataset to tests

--- tests/datasets.py
+++ tests/datasets.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import PIL.Image as Image
 
 def regframes(name):
     """Load the *name* registration dataset and return source and reference frame."""
@@ -9,3 +10,14 @@ def regframes(name):
 def lena():
     """Return Lena in all her glory."""
     return np.load(os.path.join(os.path.dirname(__file__), 'lena.npz'))['lena']
+
+def traffic_hd():
+    """Return a 1080p image of some traffic as a floating point greyscale image."""
+    return np.asarray(Image.open(
+        os.path.join(os.path.dirname(__file__), 'traffic_hd.jpg')).convert('L')) / 255.0
+
+def traffic_hd_rgb():
+    """Return a 1080p image of some traffic as a floating point RGB image."""
+    return np.asarray(Image.open(
+        os.path.join(os.path.dirname(__file__), 'traffic_hd.jpg'))) / 255.0
+

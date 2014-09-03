26492465ff464888eb117efedb05dff8c6c05f40
Rich Wareham <rjw57@cam.ac.uk>
2014-09-03 21:21:58 +0100

    opencl: set chunk size dynamically depending on device capability

--- dtcwt/opencl/convolve.py
+++ dtcwt/opencl/convolve.py
@@ -30,6 +30,15 @@ def _global_and_local_size(output_shape, chunk_size):
     global_size = tuple(int(x) for x in _ceil_multiple(output_shape[:2], local_size))
     return global_size, local_size
 
+def _good_chunk_size_for_queue(queue):
+    # By default use _DEFAULT_CHUNK_SIZE for chunk size but reduce it if
+    # the device does not support a work group that big.
+    chunk_size = _DEFAULT_CHUNK_SIZE
+    if chunk_size * chunk_size > queue.device.max_work_group_size:
+        # Set chunk size to largest which will fit
+        chunk_size = int(np.floor(np.sqrt(queue.device.max_work_group_size)))
+    return chunk_size
+
 def _build_program(queue, kernel_half_width, chunk_size):
     """Load and build the convolution kernel program for a specified kernel
     half width and chunk size. Returns a cl.Program object on success.
@@ -45,9 +54,10 @@ def _build_program(queue, kernel_half_width, chunk_size):
     return program
 
 def _write_input_pixel_test_image(queue, output_array, input_offset, input_shape, wait_for=None):
-    program = _build_program(queue, 0, _DEFAULT_CHUNK_SIZE)
+    chunk_size = _good_chunk_size_for_queue(queue)
+    program = _build_program(queue, 0, chunk_size)
     out_data, out_offset, out_strides, out_shape = _array_to_spec(output_array)
-    global_size, local_size = _global_and_local_size(output_array.shape, _DEFAULT_CHUNK_SIZE)
+    global_size, local_size = _global_and_local_size(output_array.shape, chunk_size)
     return program.test_edge_reflect(queue, global_size, local_size,
             cla.vec.make_int2(*input_offset), cla.vec.make_int2(*input_shape),
             out_data, out_offset, out_strides, out_shape, wait_for=wait_for)
@@ -85,7 +95,7 @@ class Convolution1D(object):
         self._kernel_coeffs = cla.to_device(queue, kernel_coeffs)
 
         # Compute chunk size, kernel half width and build device program
-        self._chunk_size = _DEFAULT_CHUNK_SIZE
+        self._chunk_size = _good_chunk_size_for_queue(queue)
         self._kernel_half_width = (kernel_coeffs.shape[0] - 1)>>1
         self._program = _build_program(queue, self._kernel_half_width, self._chunk_size)
 

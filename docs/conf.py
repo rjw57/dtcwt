976bdcff402be2df785c4b62c06300e1ff51b552
Rich Wareham <rjw57@cam.ac.uk>
2015-08-05 11:14:47 +0100

    docs: move to new default sphinx template

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
+if os.environ.get('READTHEDOCS', None) == 'True':
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
